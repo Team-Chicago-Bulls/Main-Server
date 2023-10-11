69780b0cc070c46fc5fb4245a52fa5efd806f8db
JossyrUpM <jossyr.pinto.2020@upb.edu.co>
2023-10-11 18:48:42 -0500

    Deleting some files.

--- server/src/main/java/com/soap/interfaces/RMIServiceAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.soap.interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RMIServiceAdapter extends Remote {
-    void uploadFileToNode(String filePath, byte[] fileData) throws RemoteException;
-}
